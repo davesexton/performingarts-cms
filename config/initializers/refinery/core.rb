141d9a7d0302841099f0db50ce72ebec455046fd
Dave Sexton <davesexton1@googlemail.com>
2014-03-31 08:20:10 +0100

    added Google Analytics

--- config/initializers/refinery/core.rb
+++ config/initializers/refinery/core.rb
@@ -25,7 +25,7 @@ Refinery::Core.configure do |config|
   # This activates Google Analytics tracking within your website. If this
   # config is left blank or set to UA-xxxxxx-x then no remote calls to
   # Google Analytics are made.
-  config.google_analytics_page_code = "UA-6202697-2"
+  config.google_analytics_page_code = "UA-49423688-2"
 
   # Enable/disable authenticity token on frontend
   # config.authenticity_token_on_frontend = false
